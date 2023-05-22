39ab6808184a3aa44180a2999fc3e3d0c9b52316
Gio Lee <giolee8@protonmail.com>
2023-05-21 22:39:22 -0400

    Add files via upload

--- resy_bot.py
+++ resy_bot.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.action_chains import ActionChains
 import logging
 from time import sleep
-from .auth import email, password
+from auth import email, password
+from datetime import datetime
+
+RESTAURANT_URL = "https://resy.com/cities/ny/don-angie?date=2023-05-21&seats=4"
 
 driver = webdriver.Firefox()
-driver.get("https://resy.com/cities/ny/don-angie?date=2023-05-21&seats=4")
+driver.get(RESTAURANT_URL)
+driver.set_window_size(3000, 3000)
 
 # Open login modal
 header = driver.find_element(By.CLASS_NAME, "MenuContainer")
@@ -18,7 +23,6 @@ for button in buttons:
 if not open_login_modal_button: 
     raise Exception("Button to open login modal not found!")
 open_login_modal_button.click() 
-sleep(3)
 
 # Go to login with email/password
 login_modal = driver.find_element(By.CLASS_NAME, "AuthView")
@@ -31,7 +35,7 @@ for button in login_modal_buttons:
 if not login_with_email_button: 
     raise Exception("Login with email button not found!")
 login_with_email_button.click()
-sleep(3)
+sleep(2)
 
 # Login to Resy 
 login_form = driver.find_element(By.CLASS_NAME, "LoginForm")
@@ -39,11 +43,67 @@ email_input = login_form.find_element(By.ID, 'email').send_keys(email)
 password_input = login_form.find_element(By.ID, 'password').send_keys(password)
 logging.info("Email and password has been filled out.")
 login_buttons = login_form.find_elements(By.XPATH, "//button[@type='submit']")
-login_button = login_buttons[0] if len(login_buttons) > 0 else None 
-if not login_button: 
+if len(login_buttons) == 0: 
     raise Exception("Login button not found!")
-sleep(3)
-login_button.click()
-sleep(3)
+login_buttons[0].click()
+sleep(2)
+
+# Wait till 9:00 AM 
+now_time = datetime.now()
+opening_timestamp = now_time.replace(hour=9, minute=0, second=0, microsecond=0).timestamp()
+now_timestamp = now_time.timestamp()
+time_delta = opening_timestamp - now_timestamp
+if time_delta < 0: 
+    raise Exception("Opening time was in the past!")
+sleep(time_delta)
+
+
+# Open reservation modal
+'''
+date_lists = driver.find_elements(By.CLASS_NAME, "ReservationButtonList")
+date_list = date_lists[0] if date_lists else None
+if not date_list:
+    raise Exception("Reservation list not found!")
+'''
+date_buttons = driver.find_elements(By.CLASS_NAME, "ReservationButton")
+print(len(date_buttons))
+date_button = None
+for button in date_buttons:
+    if int(button.get_attribute("id").split("/")[-3].split(":")[-3]) >= 17:
+        date_button = button
+        break
+if not date_button: 
+    raise Exception("No date found!")
+actions = ActionChains(driver)
+actions.move_to_element(date_button).perform()
+date_button.click()
+sleep(2)
+
+# Reserve 
+iframes = driver.find_elements(By.XPATH, "//iframe")
+found_iframe = None
+for iframe in iframes: 
+    if iframe.get_attribute("src").startswith("https://widgets.resy.com"):
+        found_iframe = iframe
+if not found_iframe: 
+    raise Exception("Reservation modal not found!") 
+driver.switch_to.frame(found_iframe)
+reservation_footers = driver.find_elements(By.CLASS_NAME, "WidgetPageFooter")
+if len(reservation_footers) == 0: 
+    raise Exception("Reservation modal footer not found!")
+reserve_buttons = reservation_footers[0].find_elements(By.XPATH, "//button")
+found_reserve_button = None
+for reserve_button in reserve_buttons: 
+    if "Reserve" in reserve_button.text:
+        found_reserve_button= reserve_button
+if not found_reserve_button:
+    raise Exception("Reserve button not found!")
+found_reserve_button.click()
+for confirm_button in reserve_buttons: 
+    if "Confirm" in confirm_button.text:
+        confirm_button.click()
+        break
+logging.info("Success!")
+sleep(5)
 
 driver.quit()
